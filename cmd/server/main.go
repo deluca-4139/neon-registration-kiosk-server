d5368cc46eb4b1b5a59682a428179516b10df524
Rosemary <rosemary@thecspc>
2023-12-14 00:09:48 -0800

    Commit prior to edits for event info

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"encoding/base64"
+	"io/ioutil"
 	"net/http"
+
+	// "strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
+
+var client = &http.Client{}
 
 func main() {
 	r := chi.NewRouter()
+
 	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Get("/", landingPage)
 	r.Get("/form.js", func(w http.ResponseWriter, r *http.Request) {
@@ -26,5 +44,15 @@ func landingPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func verifyRegistration(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("registration valid!"))
+	req, _ := http.NewRequest("GET", "https://api.neoncrm.com/v2/events", nil)
+	req.Header.Add("NEON-API-VERSION", "2.6")
+	auth_string := []byte("orgId:secret_key")
+	encoded_auth := base64.StdEncoding.EncodeToString(auth_string)
+	req.Header.Add("Authorization", "Basic "+encoded_auth)
+
+	resp, _ := client.Do(req)
+
+	w.WriteHeader(resp.StatusCode)
+	b, _ := ioutil.ReadAll(resp.Body)
+	w.Write(b)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module thecspc.org/registration
 
 go 1.13
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/go-chi/cors v1.2.1 // indirect
+)
