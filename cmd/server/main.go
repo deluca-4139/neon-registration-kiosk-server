3e341b195f8a18444b1ffb6306f6d5526cb784e0
Rosemary <rosemary@thecspc>
2023-12-14 19:34:11 -0800

    JSON decoding

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,11 +2,34 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
+	"time"
 )
+
+type Event struct {
+	ID          int32  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	StartDate   string `json:"startDate"`
+	StartTime   string `json:"startTime"`
+	EndDate     string `json:"endDate"`
+	EndTime     string `json:"endTime"`
+}
+
+type Pagination struct {
+	CurrentPage  int32 `json:"currentPage"`
+	PageSize     int32 `json:"pageSize"`
+	TotalPages   int32 `json:"totalPages"`
+	TotalResults int32 `json:"totalResults"`
+}
+
+type EventRequest struct {
+	Events     []Event    `json:"events"`
+	Pagination Pagination `json:"pagination"`
+}
 
 var client = &http.Client{}
 
@@ -22,7 +45,8 @@ func landingPage(w http.ResponseWriter, r *http.Request) {
 func verifyRegistration(w http.ResponseWriter, r *http.Request) {
 	u, _ := url.Parse("https://api.neoncrm.com/v2/events")
 	q := u.Query()
-	q.Set("startDateAfter", "2023-11-01")
+	q.Set("startDateAfter", time.Now().Format(time.DateOnly))
+	q.Set("startDateBefore", time.Now().Add(time.Hour*24).Format(time.DateOnly))
 	u.RawQuery = q.Encode()
 
 	req, _ := http.NewRequest("GET", u.String(), nil)
@@ -31,9 +55,21 @@ func verifyRegistration(w http.ResponseWriter, r *http.Request) {
 	encoded_auth := base64.StdEncoding.EncodeToString(auth_string)
 	req.Header.Add("Authorization", "Basic "+encoded_auth)
 
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+
+	var msg EventRequest
+	var decResp []byte
+
+	if err != nil {
+		// do something to fix error
+	} else if resp.StatusCode != 200 {
+		// do something to note non-200 response
+	} else {
+		json.NewDecoder(resp.Body).Decode(&msg)
+		decResp, _ = json.Marshal(msg)
+		fmt.Printf(string(decResp))
+	}
 
 	w.WriteHeader(resp.StatusCode)
-	b, _ := ioutil.ReadAll(resp.Body)
-	w.Write(b)
+	w.Write(decResp)
 }
