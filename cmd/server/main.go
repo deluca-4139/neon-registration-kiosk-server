74d5d2eeb6042ba84da8d50b3c6809d66eb1f2e2
Rosemary <rosemary@thecspc>
2023-12-14 16:19:58 -0800

    Add configuration file functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,40 +2,15 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	// "strings"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
 )
 
 var client = &http.Client{}
 
 func main() {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-	r.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-
-	r.Get("/", landingPage)
-	r.Get("/form.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "web/static/form.js")
-	})
-	r.Post("/verify", verifyRegistration)
-
-	http.ListenAndServe(":3000", r)
+	rootCmd.Execute()
 }
 
 func landingPage(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +21,7 @@ func landingPage(w http.ResponseWriter, r *http.Request) {
 func verifyRegistration(w http.ResponseWriter, r *http.Request) {
 	req, _ := http.NewRequest("GET", "https://api.neoncrm.com/v2/events", nil)
 	req.Header.Add("NEON-API-VERSION", "2.6")
-	auth_string := []byte("orgId:secret_key")
+	auth_string := []byte(fmt.Sprintf("orgId:%v", neonKey))
 	encoded_auth := base64.StdEncoding.EncodeToString(auth_string)
 	req.Header.Add("Authorization", "Basic "+encoded_auth)
 
---
+++ cmd/server/root.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var neonKey, cfgFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&neonKey, "neonkey", "", "API key for making requests to Neon CRM backend")
+	viper.BindPFlag("neonkey", rootCmd.PersistentFlags().Lookup("neonkey"))
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(".")
+		viper.SetConfigName("cobra")
+		viper.SetConfigType("yaml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Backend for automated CSPC registration",
+	Run: func(cmd *cobra.Command, args []string) {
+		neonKey = viper.Get("neonkey").(string)
+		r := chi.NewRouter()
+
+		r.Use(middleware.Logger)
+		r.Use(cors.Handler(cors.Options{
+			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins: []string{"https://*", "http://*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: false,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		}))
+
+		r.Get("/", landingPage)
+		r.Get("/form.js", func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, "web/static/form.js")
+		})
+		r.Post("/verify", verifyRegistration)
+
+		http.ListenAndServe(":3000", r)
+	},
+}
